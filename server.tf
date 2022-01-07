5a2f486ce037b74f21a92de54cab2a11d9cbd537
simpleparab <71583881+simpleparab@users.noreply.github.com>
2022-01-07 15:56:56 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -1,9 +1,6 @@
 provider "aws" {
   region     = "us-east-2"
-  access_key = "AKIAZ3S6MYW2XZY3B5UE"
-  secret_key = "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"
 }
-
 
 resource "aws_instance" "DevOpsEc2" {
   ami           = "ami-002068ed284fb165b"
