d12b92a2d358502ecd4f77e5b1c39c8aca4be72a
simpleparab <71583881+simpleparab@users.noreply.github.com>
2022-01-07 15:55:54 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -8,6 +8,7 @@ provider "aws" {
 resource "aws_instance" "DevOpsEc2" {
   ami           = "ami-002068ed284fb165b"
   instance_type = "t2.micro"
+  key_name  = "parabtest"
 
   tags = {
     Name = "ParabDevOps-Ec2"
